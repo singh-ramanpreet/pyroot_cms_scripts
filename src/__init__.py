d9187203f5979195f1332c8742f55a41ee50a730
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2019-09-16 10:15:23 -0500

    changes for py3

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
-from CMS_style import CMS_style
-from CMS_text import CMS_text
+from __future__ import absolute_import
+from .CMS_style import CMS_style
+from .CMS_text import CMS_text
 
 __all__ = ["CMS_style", "CMS_text"]
