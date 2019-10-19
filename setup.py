6efb8dffaf78aefa16ecdf298e94a8d57cdd2b5a
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2019-10-19 13:26:24 +0200

    moving toward proper packaging

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="pyroot_cms_scripts",
-    version="0.1",
+    version="0.2",
     author="Ramanpreet Singh",
     author_email="",
     description="CMS Style and Text for pyROOT",
     long_description=readme,
     url="https://github.com/singh-ramanpreet/pyroot_cms_scripts",
-    package_dir={"pyroot_cms_scripts": "src"},
     packages=["pyroot_cms_scripts"],
     scripts=[],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Unix",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Utilities"
     ],
 )
---
+++ test/test_style_text.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import ROOT
+
+from pyroot_cms_scripts import CMS_style
+
+from pyroot_cms_scripts import CMS_text
+
+
